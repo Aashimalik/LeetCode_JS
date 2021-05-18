942d16df42cc9be58f69116bdb32bd85d387bb51
Aashimalik <ayeshamalik8751@gmail.com>
2021-05-18 13:12:46 +0530

    LC:125 DONE

--- LC125_isPalindrome/index.js
+++ LC125_isPalindrome/index.js
@@ -1,4 +1,17 @@
-function isPalindrome(s) {}
+function isPalindrome(s) {
+  // Sanitize the input string
+  s = s.toLowerCase().replace(/[\W_]/g, "");
+  let left = 0;
+  let right = s.length -1;
+  while(left < right){
+    if(s[left] !== s[right]){
+      return false;
+    }
+    left++;
+    right--;
+  }
 
+  return true;
+}
 
 module.exports = isPalindrome;
